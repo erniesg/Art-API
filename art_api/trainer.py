2c8c1b0bfb506440e17ed4b46f52fa1db0aeb124
Ernie <hello@ernie.sg>
2022-09-23 00:35:00 +0800

    updating notebooks

--- art_api/trainer.py
+++ art_api/trainer.py
@@ -18,7 +18,7 @@ class BaseModel(ABC):
     Unfreeze layer
     Compile
     Fit
-    Evaluate: accuracy, classification report, confusion matrix
+    Evaluate: accuracy, losses, classification report, confusion matrix
     Predict with different thresholds (0.5, mean, median)
     Save model
     Df to csv
@@ -32,14 +32,22 @@ class BaseModel(ABC):
 
     @abstractmethod
     def build(self):
+        """model.compile
+        """
         pass
 
     @abstractmethod
     def train(self):
+        """model.fit
+        """
         pass
 
     @abstractmethod
     def evaluate(self):
+        pass
+    
+    @abstractmethod
+    def predict(self):
         pass
 
 class BaselineModel(BaseModel):
@@ -54,8 +62,19 @@ class BaselineModel(BaseModel):
         # self._preprocess_data()
 
     def build(self):
+        """Builds the Keras model"""
 
         self.model = tf.keras.Model(inputs=inputs, outputs=x)
+        
+        layer_names = [
+            "base_layer",
+            "flatten_layer",
+            "dense_layer",
+            "prediction_layer",
+        ]
+        layers = [self.base_model.get_layer(name).output for name in layer_names]
+        
+        
 
     def train(self):
         self.model.compile(optimizer=self.config.train.optimizer.type,
@@ -75,3 +94,35 @@ class BaselineModel(BaseModel):
             predictions.append(self.model.predict(image))
 
         return predictions
+
+class VGG16(BaseModel):
+  def __init__(self):
+    super(LenetModel, self).__init__()
+
+    self.feature_extractor = FeatureExtractor(8, 3, 1, "valid", "relu", 2)
+
+    self.flatten = Flatten()
+
+    self.dense_1 = Dense(100, activation = "relu")
+    self.batch_1 = BatchNormalization()
+
+    self.dense_2 = Dense(10, activation = "relu")
+    self.batch_2 = BatchNormalization()
+
+    self.dense_3 = Dense(1, activation = "sigmoid")
+    
+  def call(self, x, training):
+
+    x = self.feature_extractor(x)
+    x = self.flatten(x)
+    x = self.dense_1(x)
+    x = self.batch_1(x)
+    x = self.dense_2(x)
+    x = self.batch_2(x)
+    x = self.dense_3(x)
+
+    return x
+    
+lenet_sub_classed = LenetModel()
+lenet_sub_classed(tf.zeros([1,224,224,3]))
+lenet_sub_classed.summary()
