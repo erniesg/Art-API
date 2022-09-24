a23510b4e41cae4e84537071aef56afdc1281161
Enjiao Chen <hello@ernie.sg>
2022-09-24 16:21:59 +0800

    updating trainer and utils

--- art_api/trainer.py
+++ art_api/trainer.py
@@ -2,10 +2,93 @@ from art_api import config
 import pandas as pd
 import os
 import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, InputLayer, BatchNormalization, Input, Dropout, RandomFlip, RandomRotation, Resizing, Rescaling
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import BinaryAccuracy, FalsePositives, FalseNegatives, TruePositives, TrueNegatives, Precision, Recall, AUC, binary_accuracy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import Callback, CSVLogger, EarlyStopping, LearningRateScheduler, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.regularizers  import L2, L1
+from tensorboard.plugins.hparams import api as hp
 from abc import ABC, abstractmethod
 
 # -*- coding: utf-8 -*-
-'''Abstract base model'''
+
+"""Use functional API to write all models
+    Get X and y
+    Resize, load as array
+    Preprocess input
+    Get_config: hyperparameter tuning
+    Model: baseline, VGG16, VGG19, ResNet, Inception, Xception
+        Shared layers: rescaling, augmentation, flatten, dense, prediction
+    Unfreeze layer
+    Compile
+    Fit
+    Evaluate: accuracy, losses, classification report, confusion matrix
+    Predict with different thresholds (0.5, mean, median)
+    Save model
+    Df to csv
+"""
+
+func_input = Input(shape = (config.IM_SIZE, config.IM_SIZE, 3), name = "Input image")
+
+x = Rescaling(1./255)
+x = Conv2D(filters = 16, kernel_size = 10, activation = 'relu')(func_input)
+x = MaxPool2D(pool_size = 3)(x)
+
+x = Conv2D(filters = 32, kernel_size = 32, activation = 'relu')(func_input)
+x = MaxPool2D(pool_size = 3)(x)
+
+x = Conv2D(filters = 32, kernel_size = 32, activation = 'relu')(func_input)
+x = MaxPool2D(pool_size = 3)(x)
+
+x = Flatten()(x)
+x = Dense(100, activation="relu")
+x = Dense(10, activation="sigmoid")
+
+x = Conv2D(filters = 6, kernel_size = 3, strides=1, padding='valid', activation = 'relu')(func_input)
+x = BatchNormalization()(x)
+x = MaxPool2D (pool_size = 2, strides= 2)(x)
+
+x = Conv2D(filters = 16, kernel_size = 3, strides=1, padding='valid', activation = 'relu')(x)
+x = BatchNormalization()(x)
+output = MaxPool2D (pool_size = 2, strides= 2)(x)
+
+feature_extractor_model = Model(func_input, output, name = "Feature_Extractor")
+feature_extractor_model.summary()
+feature_extractor_seq_model = tf.keras.Sequential([
+                             InputLayer(input_shape = (IM_SIZE, IM_SIZE, 3)),
+
+                             Conv2D(filters = 6, kernel_size = 3, strides=1, padding='valid', activation = 'relu'),
+                             BatchNormalization(),
+                             MaxPool2D (pool_size = 2, strides= 2),
+
+                             Conv2D(filters = 16, kernel_size = 3, strides=1, padding='valid', activation = 'relu'),
+                             BatchNormalization(),
+                             MaxPool2D (pool_size = 2, strides= 2),
+
+                             
+
+])
+feature_extractor_seq_model.summary()
+
+func_input = Input(shape = (IM_SIZE, IM_SIZE, 3), name = "Input Image")
+
+x = feature_extractor_seq_model(func_input)
+
+x = Flatten()(x)
+
+x = Dense(100, activation = "relu")(x)
+x = BatchNormalization()(x)
+
+x = Dense(10, activation = "relu")(x)
+x = BatchNormalization()(x)
+
+func_output = Dense(1, activation = "sigmoid")(x)
+
+lenet_model_func = Model(func_input, func_output, name = "Lenet_Model")
+lenet_model_func.summary()
 
 class BaseModel(ABC):
     '''Abstract Model class that is inherited to all models
